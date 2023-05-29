fbf3862899129b2f2ff8c2ac4930ae559eef2b26
Linus <linuslee2000@gmail.com>
2023-05-29 18:16:15 +0800

    feat: upload to supabase test

--- db.py
+++ db.py
@@ -7,3 +7,9 @@ load_dotenv()
 url: str = os.environ.get("SUPABASE_PROJECT_URL")
 key: str = os.environ.get("SUPABASE_PROJECT_ANON_KEY")
 supabase: Client = create_client(url, key)
+
+source = "images/hero.jpg"
+
+with open(source, 'rb+') as f:
+  res = supabase.storage.from_('photos').upload("ab.jpg", os.path.abspath(source))
+  print(res.json())
