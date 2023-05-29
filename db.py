096bb502c02861936d1701eb26f0a91dabcff34d
SeekSaveServe <peichengyi@gmail.com>
2023-05-29 17:12:32 +0800

    Add supabase

---
+++ db.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_PROJECT_URL")
+key: str = os.environ.get("SUPABASE_PROJECT_ANON_KEY")
+supabase: Client = create_client(url, key)
