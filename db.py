e8dfec06860695bd6906c97182d2a643b7b77eb2
Linus <linuslee2000@gmail.com>
2023-05-30 07:36:18 +0800

    refactor: add the link to the photo instead of the uuid

--- db.py
+++ db.py
@@ -37,7 +37,7 @@ async def image(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Updates photos table of location
     supabase.table('photos').insert({
         "location_id": data[1][0]["location_id"],
-        "url": unique_id
+        "url": convert_uuid_to_url(unique_id)
     }).execute()
 
     os.remove(temp_file_name)
